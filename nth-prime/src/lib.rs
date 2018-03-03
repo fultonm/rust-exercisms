c6412e3d7a115bb355dee790471c25157106273c
Michael Fulton <mike@michaelfulton.co>
2018-03-02 17:44:47 -0800

    nth-prime tests pass

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -2,31 +2,27 @@ pub fn nth(n: usize) -> Result<usize, &'static str> {
     if n < 1 {
         return Err("Need to take at least the 1st prime");
     }
-    let size: usize = (2 as f64 * n as f64 * (n as f64).ln()).ceil() as usize + 15; // Set an upper bound for seiving.
+    // Set an upper bound for seiving, ensure size is at least 4 so we can get
+    // a square root calculation.
+    let size: usize = (2 as f64 * n as f64 * (n as f64).ln()).ceil() as usize + 4;
     let size_sqrt: usize = (size as f64).sqrt().ceil() as usize;
     let mut nums: Vec<usize> = vec![0; size];
-    println!("nums length is {0}", nums.len());
+    for i in 0..size {
+        nums[i] = i;
+    }
     let primes: Vec<usize> = sieve(&mut nums, size, size_sqrt);
-    println!("primes length is {0}", primes.len());
-    return Ok(*primes.last().unwrap());
+    return Ok(primes[n - 1]);
 }
 
 fn sieve(nums: &mut [usize], size: usize, size_sqrt: usize) -> Vec<usize> {
     for i in 0..size {
-        nums[i] = i;
-    }
-    for i in 0..size {
-        println!("nums[{0}] = {1}", i, nums[i])
-    }
-    for i in 0..size {
         if i < 2 {
+            nums[i] = 0;
             continue;
         } else if nums[i] > size_sqrt {
             break;
         }
-        let step = nums[i];
-        println!("This iteration #{0}, nums[i] is {1}", i, nums[i]);
-        for j in (nums[i].pow(2)..size).filter(|x| x % step == 0) {
+        for j in (nums[i].pow(2)..size).filter(|x| x % i == 0) {
             nums[j] = 0;
         }
     }
--- nth-prime/tests/nth-prime.rs
+++ nth-prime/tests/nth-prime.rs
@@ -6,25 +6,26 @@ fn test_first_prime() {
 }
 
 #[test]
-#[ignore]
 fn test_second_prime() {
     assert_eq!(np::nth(2), Ok(3));
 }
 
 #[test]
-#[ignore]
 fn test_sixth_prime() {
     assert_eq!(np::nth(6), Ok(13));
 }
 
 #[test]
-#[ignore]
 fn test_big_prime() {
     assert_eq!(np::nth(10001), Ok(104743));
 }
 
 #[test]
-#[ignore]
+fn test_huge_prime() {
+    assert_eq!(np::nth(99999), Ok(1299689));
+}
+
+#[test]
 fn test_zeroth_prime() {
     assert!(np::nth(0).is_err());
 }
