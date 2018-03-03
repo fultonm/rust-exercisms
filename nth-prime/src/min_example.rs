cca26005ea23248edf58b365eb00c0193f4c1657
Michael Fulton <mike@michaelfulton.co>
2018-03-02 19:03:55 -0800

    sync changes

--- nth-prime/src/min_example.rs
+++ nth-prime/src/min_example.rs
@@ -1,10 +1,28 @@
+use std::thread;
+use std::time::Duration;
+use std::sync::mpsc;
+
 pub fn main() {
-    let len = 10;
-    let mut zero_vec = vec![0; len];
-    for i in 0..len {
-        for j in (0..len).filter(|x| x % i == 0) {
-            zero_vec[i] = j;
+    let (tx, rx) = mpsc::channel();
+    let tx1 = mpsc::Sender::clone(&tx);
+
+    thread::spawn(move || {
+        let msgs = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+        for msg in msgs {
+            tx.send(msg).unwrap();
+            thread::sleep(Duration::from_millis(msg * 30));
         }
+    });
+
+    thread::spawn(move || {
+        let msgs = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+        for msg in msgs {
+            tx1.send(msg).unwrap();
+            thread::sleep(Duration::from_millis(msg * 20));
+        }
+    });
+
+    for received in rx {
+        println!("MAIN received {0}", received);
     }
-    
 }
