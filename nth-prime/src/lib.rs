080fd1fb72ec91896bd9503cd7de5f7b2e713c24
Michael Fulton <mike@michaelfulton.co>
2018-03-02 17:01:25 -0800

    commit changes for pull

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,17 +1,13 @@
-extern crate itertools;
-use itertools::Itertools;
-
 pub fn nth(n: usize) -> Result<usize, &'static str> {
     if n < 1 {
         return Err("Need to take at least the 1st prime");
     }
     let size: usize = (2 as f64 * n as f64 * (n as f64).ln()).ceil() as usize + 15; // Set an upper bound for seiving.
-    println!("Size is {0}", size);
     let size_sqrt: usize = (size as f64).sqrt().ceil() as usize;
-    let mut nums: Vec<usize> = vec![0, size];
-    println!("Nums size is {0}", nums.len());
+    let mut nums: Vec<usize> = vec![0; size];
+    println!("nums length is {0}", nums.len());
     let primes: Vec<usize> = sieve(&mut nums, size, size_sqrt);
-    println!("Elements in primes: {0}", primes.len());
+    println!("primes length is {0}", primes.len());
     return Ok(*primes.last().unwrap());
 }
 
@@ -20,14 +16,22 @@ fn sieve(nums: &mut [usize], size: usize, size_sqrt: usize) -> Vec<usize> {
         nums[i] = i;
     }
     for i in 0..size {
+        println!("nums[{0}] = {1}", i, nums[i])
+    }
+    for i in 0..size {
         if i < 2 {
             continue;
         } else if nums[i] > size_sqrt {
             break;
         }
-        for j in (nums[i].pow(2)..size).step(nums[i]) {
+        let step = nums[i];
+        println!("This iteration #{0}, nums[i] is {1}", i, nums[i]);
+        for j in (nums[i].pow(2)..size).filter(|x| x % step == 0) {
             nums[j] = 0;
         }
     }
-    return nums.iter().filter(|&x| *x != 0).map(|&x| x as usize).collect();
+    return nums.iter()
+        .filter(|&x| *x != 0)
+        .map(|&x| x as usize)
+        .collect();
 }
--- nth-prime/src/min_example.rs
+++ nth-prime/src/min_example.rs
@@ -1,2 +1,10 @@
 pub fn main() {
+    let len = 10;
+    let mut zero_vec = vec![0; len];
+    for i in 0..len {
+        for j in (0..len).filter(|x| x % i == 0) {
+            zero_vec[i] = j;
+        }
+    }
+    
 }
